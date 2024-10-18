8460b3b4c9528f393fe3875e9be43ac267b259cb
leonardoRuglio <95985145+leonardoRuglio@users.noreply.github.com>
2024-10-18 13:44:59 +0200

    Update config.php
    Remove Production Api key

--- config/config.php
+++ config/config.php
@@ -2,8 +2,6 @@
 // Define API configuration constants
 define('API_URL_NUKI_DEVICES','https://api.nuki.io/smartlock/'); // API for the Nuki devices "Stairway to Heaven"
 define('API_URL','https://api.nuki.io/smartlock/auth');
-//Production API
-//define('API_TOKEN', '92ad2f01e18dc5b53ebb4065e408cb70024ec74c7f8ef782ce27aa312a165193ce5c6323ee24aff2'); // API token for authentication
 
 //test API
 define('API_TOKEN', '28a4a46ace782c52e961595b64830909249d55796a2fc0a9d0b2f27b808631889dbad4a60a443219'); // API token for authentication
